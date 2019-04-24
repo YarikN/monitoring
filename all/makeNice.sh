9155b7f1e5b2c1c5d03904226772ea5c6be94a4e
z-vadim <z_vadim@outlook.com>
2019-04-24 17:42:30 +0300

    Update makeNice.sh

--- all/makeNice.sh
+++ all/makeNice.sh
@@ -1,12 +1,12 @@
-kubectl create -f namespace-mon.json
+kubectl create -f namespace-mon.json;
 helm repo update;
 helm init;
 sleep 2;
 helm install --namespace monitoring --name prometheus stable/prometheus --set-file extraScrapeConfigs=extraScrapeConfigs.yaml;
 sleep 2;
-kubectl create -f prometheus-config.yaml
-kubectl create -f prometheus-rules.yaml
-helm install --namespace monitoring --name grafana stable/grafana --set adminUser=admin --set adminPassword=admin
-kubectl get pods -n monitoring
-export POD_NAME=$(kubectl get pods --namespace monitoring -l "app=grafana,release=grafana" -o jsonpath="{.items[0].metadata.name}")
-kubectl --namespace monitoring port-forward $POD_NAME 3000
+kubectl create -f prometheus-config.yaml;
+kubectl create -f prometheus-rules.yaml;
+helm install --namespace monitoring --name grafana stable/grafana --set adminUser=admin --set adminPassword=admin;
+kubectl get pods -n monitoring;
+export POD_NAME=$(kubectl get pods --namespace monitoring -l "app=grafana,release=grafana" -o jsonpath="{.items[0].metadata.name}");
+kubectl --namespace monitoring port-forward $POD_NAME 3000;
