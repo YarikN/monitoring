d30957b7efd30b2ccd8a63fa3431dc0e929c4c80
z-vadim <z_vadim@outlook.com>
2019-04-24 17:41:54 +0300

    Update makeNice.sh

--- all/makeNice.sh
+++ all/makeNice.sh
@@ -6,7 +6,7 @@ helm install --namespace monitoring --name prometheus stable/prometheus --set-fi
 sleep 2;
 kubectl create -f prometheus-config.yaml
 kubectl create -f prometheus-rules.yaml
-helm install --namespace monitoring --name grafana stable/grafana --set adminUser=admin --set adminPassword=admin123 
+helm install --namespace monitoring --name grafana stable/grafana --set adminUser=admin --set adminPassword=admin
 kubectl get pods -n monitoring
 export POD_NAME=$(kubectl get pods --namespace monitoring -l "app=grafana,release=grafana" -o jsonpath="{.items[0].metadata.name}")
 kubectl --namespace monitoring port-forward $POD_NAME 3000
